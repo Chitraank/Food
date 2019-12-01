57a51c4b160dc5cdeb0bfbded52f77e493d91d4e
sanchi0204 <sanchirastogi02@gmail.com>
2019-12-02 03:23:00 +0530

    ui

--- app/src/main/java/com/example/intern_food/Sign_in_up.java
+++ app/src/main/java/com/example/intern_food/Sign_in_up.java
@@ -2,13 +2,37 @@ package com.example.intern_food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Sign_in_up extends AppCompatActivity {
+
+    Button signIn, signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_up);
+
+        signIn= findViewById(R.id.button_sign_in);
+        signUp= findViewById(R.id.button_sign_up);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Sign_in_up.this,Login.class);
+                startActivity(i);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Sign_in_up.this,Signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
