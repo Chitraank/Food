7896d95d0ae359d359bb09625395a38bbddddd42
sanchi0204 <sanchirastogi02@gmail.com>
2019-12-01 21:47:30 +0530

    ui added

---
+++ app/src/main/java/com/example/intern_food/Meals.java
@@ -0,0 +1,14 @@
+package com.example.intern_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meals);
+    }
+}
---
+++ app/src/main/java/com/example/intern_food/Sign_in_up.java
@@ -0,0 +1,14 @@
+package com.example.intern_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_in_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_up);
+    }
+}
