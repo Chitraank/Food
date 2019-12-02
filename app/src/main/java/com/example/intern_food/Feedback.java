ff98538520ac1c90fb2656a54c541c89c92ee351
sanchi0204 <sanchirastogi02@gmail.com>
2019-12-03 00:04:33 +0530

    ui corrected

--- app/src/main/java/com/example/intern_food/Feedback.java
+++ app/src/main/java/com/example/intern_food/Feedback.java
@@ -1,14 +1,70 @@
 package com.example.intern_food;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 
 public class Feedback extends AppCompatActivity {
+
+    CardView brkfast, lunch, snacks, dinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        brkfast = findViewById(R.id.card_breakfast_item);
+        lunch = findViewById(R.id.card_lunch_item);
+        snacks = findViewById(R.id.card_snack_item);
+        dinner = findViewById(R.id.card_dinner_item);
+
+
+        brkfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new FeedbackFragment());
+
+            }
+        });
+
+        lunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loadFragment(new FeedbackFragment());
+            }
+        });
+
+        snacks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new FeedbackFragment());
+
+            }
+        });
+
+        dinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new FeedbackFragment());
+
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+// create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+// create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+// replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.feedback_fragment, fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 }
