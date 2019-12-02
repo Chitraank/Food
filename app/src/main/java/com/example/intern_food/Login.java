82429cbcd5334c13f25302e03c61d15cc5aad287
amitabhsharma <amitabhs30@gmail.com>
2019-12-02 17:28:48 +0530

    Backend Work

--- app/src/main/java/com/example/intern_food/Login.java
+++ app/src/main/java/com/example/intern_food/Login.java
@@ -8,8 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
@@ -26,12 +30,32 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signIn= findViewById(R.id.btn_sign_in);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Login.this, Signup.class);
-                startActivity(i);
-            }
-        });
+    }
+    public void signIn(View view)
+    {
+        email=Email.getText().toString();
+        password=Password.getText().toString();
+
+        if(!(email.isEmpty()||password.isEmpty()))
+        {
+            mAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(Login.this, "SignedIn", Toast.LENGTH_SHORT).show();
+
+                                startActivity(new Intent(Login.this,Dashboard.class));
+                            }
+                            else
+                                Toast.makeText(Login.this, "Error:"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+        else
+        {
+            Toast.makeText(this, "Invalid Fields", Toast.LENGTH_SHORT).show();
+        }
     }
 }
