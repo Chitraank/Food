cd5f727102765ddfd945334b3d5d80de1cd7ac9b
amitabhsharma <amitabhs30@gmail.com>
2019-12-03 15:53:10 +0530

    Merge branch 'master' of /Users/amitabhsharma/StudioProjects/Food with conflicts.

--- app/src/main/java/com/example/intern_food/Feedback.java
+++ app/src/main/java/com/example/intern_food/Feedback.java
@@ -1,5 +1,6 @@
 package com.example.intern_food;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -8,15 +9,45 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Feedback extends AppCompatActivity {
 
     CardView brkfast, lunch, snacks, dinner;
+    FirebaseAuth mAuth;
+    RadioGroup brgrp,lngrp,sngrp,dngrp;
+    RadioButton radioButton1,radioButton2,radioButton3,radioButton4;
+    DatabaseReference feedbackref= FirebaseDatabase.getInstance().getReference().child("feedback");
+    private String formattedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        brgrp=findViewById(R.id.brGrp);
+        lngrp=findViewById(R.id.lnGrp);
+        sngrp=findViewById(R.id.sngrp);
+        mAuth=FirebaseAuth.getInstance();
+        dngrp=findViewById(R.id.dngrp);
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        formattedDate = df.format(c);
 
         brkfast = findViewById(R.id.card_breakfast_item);
         lunch = findViewById(R.id.card_lunch_item);
@@ -24,47 +55,98 @@ public class Feedback extends AppCompatActivity {
         dinner = findViewById(R.id.card_dinner_item);
 
 
-        brkfast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadFragment(new FeedbackFragment());
 
-            }
-        });
 
-        lunch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                loadFragment(new FeedbackFragment());
-            }
-        });
-
-        snacks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadFragment(new FeedbackFragment());
-
-            }
-        });
-
-        dinner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadFragment(new FeedbackFragment());
-
-            }
-        });
+//        brkfast.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                loadFragment(new FeedbackFragment());
+//
+//            }
+//        });
+//
+//        lunch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                loadFragment(new FeedbackFragment());
+//            }
+//        });
+//
+//        snacks.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                loadFragment(new FeedbackFragment());
+//
+//            }
+//        });
+//
+//        dinner.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                loadFragment(new FeedbackFragment());
+//
+//            }
+//        });
 
     }
 
-    private void loadFragment(Fragment fragment) {
-// create a FragmentManager
-        FragmentManager fm = getFragmentManager();
-// create a FragmentTransaction to begin the transaction and replace the Fragment
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-// replace the FrameLayout with new Fragment
-        fragmentTransaction.replace(R.id.feedback_fragment, fragment);
-        fragmentTransaction.commit(); // save the changes
+//    private void loadFragment(Fragment fragment) {
+//// create a FragmentManager
+//        FragmentManager fm = getFragmentManager();
+//// create a FragmentTransaction to begin the transaction and replace the Fragment
+//        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+//// replace the FrameLayout with new Fragment
+//        fragmentTransaction.replace(R.id.feedback_fragment, fragment);
+//        fragmentTransaction.commit(); // save the changes
+//    }
+
+    public void submitFeedback(View view)
+    {
+        int radioId1=brgrp.getCheckedRadioButtonId();
+        int radioId2=lngrp.getCheckedRadioButtonId();
+        int radioId3=sngrp.getCheckedRadioButtonId();
+        int radioId4=dngrp.getCheckedRadioButtonId();
+
+        radioButton1=findViewById(radioId1);
+        radioButton2=findViewById(radioId2);
+        radioButton3=findViewById(radioId3);
+        radioButton4=findViewById(radioId4);
+
+        final HashMap<String,Object> feedbackMap=new HashMap<>();
+
+        feedbackMap.put("Bread Butter",radioButton1.getText());
+        feedbackMap.put("Dal Roti Rice",radioButton2.getText());
+        feedbackMap.put("Burger",radioButton3.getText());
+        feedbackMap.put("Paneer Sabzi,Dal",radioButton4.getText());
+
+        feedbackref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(formattedDate).child(mAuth.getUid()).child("FB").exists())
+                {
+                    Toast.makeText(Feedback.this, "Already Submitted Feedback for today", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    feedbackref.child(formattedDate).child(mAuth.getUid()).child("FB").updateChildren(feedbackMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(Feedback.this, "Successfully Added Feedback", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
     }
 }
