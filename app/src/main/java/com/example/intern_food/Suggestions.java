e7aa3c9bdafe68c00dfbdf731fb10bd2b8925cf5
amitabhsharma <amitabhs30@gmail.com>
2019-12-03 22:21:45 +0530

    backend

--- app/src/main/java/com/example/intern_food/Suggestions.java
+++ app/src/main/java/com/example/intern_food/Suggestions.java
@@ -5,18 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Suggestions extends AppCompatActivity {
     FloatingActionButton fabSuggestions;
     ImageView img_sad, img_nuetral, img_satis, img_happy;
+    EditText title,suggestion;
+    RadioButton radioButton;
+    RadioGroup radioGroup;
+    String overallExp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestions);
+        title=findViewById(R.id.suggestion_title);
+        suggestion=findViewById(R.id.suggestion);
+        radioGroup=findViewById(R.id.radio_grp_issue_type);
 
         fabSuggestions= findViewById(R.id.fab_suggestions);
         fabSuggestions.setOnClickListener(new View.OnClickListener() {
@@ -38,5 +48,45 @@ public class Suggestions extends AppCompatActivity {
 //                img_sad.set
 //            }
 //        });
+    }
+    public void submitSuggestions(View view)
+    {
+
+        int radioId=radioGroup.getCheckedRadioButtonId();
+        radioButton=findViewById(radioId);
+
+
+    }
+    public void happy(View view)
+    {
+        overallExp="Happy";
+        img_happy.setImageResource(R.drawable.mood_happy_pink);
+        img_satis.setImageResource(R.drawable.mood_satisfy);
+        img_nuetral.setImageResource(R.drawable.mood_nuetral);
+        img_sad.setImageResource(R.drawable.mood_bad);
+    }
+    public void satis(View view)
+    {
+        overallExp="Satisfactory";
+        img_happy.setImageResource(R.drawable.mood_happy);
+        img_satis.setImageResource(R.drawable.mood_satisfy);
+        img_nuetral.setImageResource(R.drawable.mood_nuetral);
+        img_sad.setImageResource(R.drawable.mood_bad);
+    }
+    public void neut(View view)
+    {
+        overallExp="Neutral";
+        img_happy.setImageResource(R.drawable.mood_happy);
+        img_satis.setImageResource(R.drawable.mood_satisfy);
+        img_nuetral.setImageResource(R.drawable.mood_nuetral_pink);
+        img_sad.setImageResource(R.drawable.mood_bad);
+    }
+    public void sad(View view)
+    {
+        overallExp="Sad";
+        img_happy.setImageResource(R.drawable.mood_happy);
+        img_satis.setImageResource(R.drawable.mood_satisfy);
+        img_nuetral.setImageResource(R.drawable.mood_nuetral);
+        img_sad.setImageResource(R.drawable.mood_bad_pink);
     }
 }
